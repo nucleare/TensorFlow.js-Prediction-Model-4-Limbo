500431d9a8ed2d1d287e94d99a6780a626fbfc2b
chi11estpanda <29671486+nucleare@users.noreply.github.com>
2022-12-16 18:21:26 -0800

    REference

---
+++ RNG.js
@@ -0,0 +1,25 @@
+import hmacSHA256 from 'https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/hmac-sha256.js';
+
+// Random number generation based on following inputs: serverSeed, clientSeed, nonce and cursor
+function byteGenerator({ serverSeed, clientSeed, nonce, cursor }) {
+  // Setup curser variables
+  let currentRound = Math.floor(cursor / 32);
+  let currentRoundCursor = cursor;
+  currentRoundCursor -= currentRound * 32;
+
+  // Generate outputs until cursor requirement fullfilled
+  while (true) {
+    // HMAC function used to output provided inputs into bytes
+    const hmac = createHmac('sha256', serverSeed);
+    hmac.update(`${clientSeed}:${nonce}:${currentRound}`);
+    const buffer = hmac.digest();
+
+    // Update curser for next iteration of loop
+    while (currentRoundCursor < 32) {
+      yield Number(buffer[currentRoundCursor]);
+      currentRoundCursor += 1;
+    }
+    currentRoundCursor = 0;
+    currentRound += 1;
+  }
+}
